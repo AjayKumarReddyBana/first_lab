d28f8c2488ae24d29c61d178d28c006fe12bea0b
AJ <aj@AJs-MacBook-Pro.local>
2024-01-22 12:05:24 -0500

    debugged testcase file

--- tests/testcases.py
+++ tests/testcases.py
@@ -1,2 +1,20 @@
-import pytest as pytest
+import pytest
+from src import calculator
+
+
+def test1():
+    assert calculator.subtract(3,2)==1
+    assert calculator.add(3,2)==5
+    assert calculator.product(3,2)==6
+
+
+
+
+
+
+
+
+
+
+
 
